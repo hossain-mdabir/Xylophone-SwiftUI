e029b497abbf72b9ab77bde00861f9298a290685
Md Abir Hossain <hossain.mdabir@gmail.com>
2022-06-18 23:22:34 +0600

    Initial Commit

---
+++ Xylophone-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Xylophone-SwiftUI
+//
+//  Created by MD Abir Hossain on 18/6/22.
+//  Copyright © 2022 Md Abir Hossain. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Xylophone-SwiftUITests/Xylophone_SwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  Xylophone_SwiftUITests.swift
+//  Xylophone-SwiftUITests
+//
+//  Created by MD Abir Hossain on 18/6/22.
+//  Copyright © 2022 Md Abir Hossain. All rights reserved.
+//
+
+import XCTest
+@testable import Xylophone_SwiftUI
+
+class Xylophone_SwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
